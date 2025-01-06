72654a5328b8d83a0faf4d98cbf61d7300f470a0
Silvio Tremblay <sl704836@dal.ca>
2025-01-06 17:38:31 -0400

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,8 @@ import Logo from "../../public/assets/img/logo.png"
 
 const Navbar = () => {
   const [active, setActive] = useState("")
-  return (
+  const [toggle, settoggle] = useState(false)
+  rfalse(
     <nav className={`${styles.paddingX} w-full flex 
     items-center py-5 fixed top-0 z-20 bg-primary`}
     >
@@ -32,11 +33,30 @@ const Navbar = () => {
         <ul className='list-none hidden sm:flex
         flex-row gap-10'>
           {navLinks.map((link) => (
-            <li>
+            <li
+              key={link.id}
+              className={`${
+                active == link.title
+                ? "text-white"
+                : "text-secondary"
+              } hover:text-white text-[18px] 
+              font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
+
+      <div className='sm:hidden flex flex-1
+        justify-end items-center'>
+          <img src={toggle ? close : menu}
+          alt="menu" 
+          className='w-[28px] h-[28px] 
+          object-contain cursor-pointer'
+          onClick={() => setToggle(!toggle)} 
+          />
+        </div>
       </div>
     </nav>
   )
